d274b3619355cc25dfa69dedb3808bc9a2b46b2b
laksh <lakshtaneja77@gmail.com>
2024-05-28 15:32:04 +0530

    modify user data struct

--- data/user.go
+++ data/user.go
@@ -1,21 +1,21 @@
 package data
 
-type User struct {
-	Name    string // capitalizing means public
-	Id      string
-	Contact Contact
-	Status  string
+type User struct { // capitalizing means public
+	Name    string `json:"name"`
+	Id      string `json:"id"`
+	Contact `json:"contact"`
+	Status  string `json:"status"`
 }
 
 type Contact struct {
-	Phone1  string
-	Phone2  string
-	Address string
+	Phone   string `json:"phone"`
+	Email   string `json:"email"`
+	Address string `json:"address"`
 }
 
 type Student struct {
-	User User
-	CGPA float64 // json default uses float64
+	User `json:"user"`
+	Cgpa float64 // json default uses float64
 }
 
 //TODO: Add Course, Teacher, and Fee structs
