97297d6d4c915fccdd4e5bd6914ecc6c7a87b62f
laksh <lakshtaneja77@gmail.com>
2024-05-29 08:28:18 +0530

    feat: Add gorilla/mux router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lakshhtaneja/ERP
 
 go 1.22.3
+
+require github.com/gorilla/mux v1.8.1
--- home.go
+++ home.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
+
 	"github.com/lakshhtaneja/ERP/api"
 )
 
 func main() {
-	server := http.NewServeMux()
+	server := mux.NewRouter()
 
-	server.HandleFunc("/student/details", api.GetStudent)
+	server.HandleFunc("/student/details", api.GetStudent).Methods("GET")
 	err := http.ListenAndServe(":3333", server)
 	if err != nil {
 		fmt.Println("Error starting server")
